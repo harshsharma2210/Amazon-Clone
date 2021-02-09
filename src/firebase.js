ee2c254761311bfe93ac20c06f7881f8592a0fe6
Harsh <harshsharma102000@gmail.com>
2021-02-09 14:18:30 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyD8pRNZw4mc368xzhqt4JcWsWzAJbW6eho",
+    authDomain: "challenge-52bff.firebaseapp.com",
+    projectId: "challenge-52bff",
+    storageBucket: "challenge-52bff.appspot.com",
+    messagingSenderId: "109679667043",
+    appId: "1:109679667043:web:b106832adb22924b6f5556"
+  };
